e25be3a52d182374eb7a3fecedf1ccb3b2141fc2
matthotovy17 <42526675+matthotovy17@users.noreply.github.com>
2019-02-13 16:22:35 -0600

    Created a File Reader class

--- CincoComputerConsultants/src/com/cinco/InvoiceReport.java
+++ CincoComputerConsultants/src/com/cinco/InvoiceReport.java
@@ -4,6 +4,18 @@ public class InvoiceReport {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//1. in here when finding the start and end date of a license we need to use
+		// the java.time libaray and use the localDate method to find year-month-day
+		
+		//2. Make copy constructors for changing values in objects and subclasses
+		
+//		INV003;ttew;321f;900g:25,1449:3,1p94:2018-05-12:2019-05-13
+		
+		//invoiceUuid;customerUuid
+		
+		//3. follow the single responsiblilty principle 
+		// create classes for loading data, serializing data, 
 
 	}
 
