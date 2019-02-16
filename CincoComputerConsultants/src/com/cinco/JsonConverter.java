b45f8fe09b8140c1286cd01582231f39b5d280be
Chloe Galinsky <chloe@galinsky.net>
2019-02-15 21:13:21 -0600

    commented out so i can compile to jar

--- CincoComputerConsultants/src/com/cinco/JsonConverter.java
+++ CincoComputerConsultants/src/com/cinco/JsonConverter.java
@@ -1,20 +1,20 @@
-package com.cinco;
-
-public class JsonConverter {
-	
-	// TODO: Converting Map to prettyPrinting JSON format
-	public void toJson() {
-		// parameterize the toJson method
-		Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
-		FileWriter outputFile = null;
-		try {
-			outputFile = new FileWriter("data/Persons.json");
-			outputFile.write(gsonBuilder.toJson());
-			// TODO: pass in Map to toJson function
-			outputFile.close();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-	}
-}
+//package com.cinco;
+//
+//public class JsonConverter {
+//	
+//	// TODO: Converting Map to prettyPrinting JSON format
+//	public void toJson() {
+//		// parameterize the toJson method
+//		Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+//		FileWriter outputFile = null;
+//		try {
+//			outputFile = new FileWriter("data/Persons.json");
+//			outputFile.write(gsonBuilder.toJson());
+//			// TODO: pass in Map to toJson function
+//			outputFile.close();
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+//		
+//	}
+//}
