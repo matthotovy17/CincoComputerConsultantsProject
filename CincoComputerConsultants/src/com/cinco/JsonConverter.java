e810e164d23b2187488d47cfa62bd7a94ad0e0ef
Chloe Galinsky <chloe@galinsky.net>
2019-02-18 10:26:39 -0600

    uncommented it all once I created the JAR

--- CincoComputerConsultants/src/com/cinco/JsonConverter.java
+++ CincoComputerConsultants/src/com/cinco/JsonConverter.java
@@ -1,19 +1,19 @@
 package com.cinco;
 public class JsonConverter {
-//	
-//	// TODO: Converting Map to prettyPrinting JSON format
-//	public void toJson() {
-//		// parameterize the toJson method
-//		Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
-//		FileWriter outputFile = null;
-//		try {
-//			outputFile = new FileWriter("data/Persons.json");
-//			outputFile.write(gsonBuilder.toJson());
-//			// TODO: pass in Map to toJson function
-//			outputFile.close();
-//		} catch (IOException e1) {
-//			e1.printStackTrace();
-//		}
-//		
-//	}
+	
+	// TODO: Converting Map to prettyPrinting JSON format
+	public void toJson() {
+		// parameterize the toJson method
+		Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+		FileWriter outputFile = null;
+		try {
+			outputFile = new FileWriter("data/Persons.json");
+			outputFile.write(gsonBuilder.toJson());
+			// TODO: pass in Map to toJson function
+			outputFile.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+	}
 }
